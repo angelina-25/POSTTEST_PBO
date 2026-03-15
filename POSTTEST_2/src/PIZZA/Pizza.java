def85f44cc6e1f837c066751cd968f7972d9918e
angelina-25 <augustinumami46@gmail.com>
2026-03-15 15:31:27 +0800

    Add files via upload

---
+++ POSTTEST_2/src/PIZZA/Pizza.java
@@ -0,0 +1,47 @@
+package PIZZA;
+
+public class Pizza {
+
+    private String namaPizza;
+    private String ukuran;
+    private int harga;
+
+    Pizza(String namaPizza, String ukuran, int harga) {
+        this.namaPizza = namaPizza;
+        this.ukuran = ukuran;
+        this.harga = harga;
+    }
+
+    public String getNamaPizza() {
+        return namaPizza;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setNamaPizza(String namaPizza) {
+        this.namaPizza = namaPizza;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public void setHarga(int harga) {
+        if (harga > 0) {
+            this.harga = harga;
+        } else {
+            System.out.println("Harga tidak boleh negatif!");
+        }
+    }
+
+    // protected method (contoh access modifier lain)
+    protected void tampilInfoPizza() {
+        System.out.println(namaPizza + " - " + ukuran + " - Rp" + harga);
+    }
+}
